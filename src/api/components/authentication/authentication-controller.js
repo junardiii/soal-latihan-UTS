e63a70b865e3c4404e3728d49a3aa9caed2319ea
junardi chailesia <junardi.535230225@stu.untar.ac.id>
2024-04-11 22:16:15 +0700

    first commit

---
+++ src/api/components/authentication/authentication-controller.js
@@ -0,0 +1,78 @@
+const { errorResponder, errorTypes } = require('../../../core/errors');
+const authenticationServices = require('./authentication-service');
+
+/**
+ * Handle login request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function login(request, response, next) {
+  const { email, password } = request.body;
+
+  try {
+    // Check login credentials
+    const loginSuccess = await authenticationServices.checkLoginCredentials(
+      email,
+      password
+    );
+
+    if (!loginSuccess) {
+      throw errorResponder(
+        errorTypes.INVALID_CREDENTIALS,
+        'Wrong email or password'
+      );
+    }
+
+    return response.status(200).json(loginSuccess);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  login,
+};
+/**
+ * Handle create user request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function createUser(request, response, next) {
+  try {
+    const { name, email, password, password_confirm } = request.body;
+
+    // Check if password and confirm password match
+    if (password !== password_confirm) {
+      throw errorResponder(
+        errorTypes.INVALID_PASSWORD,
+        'Password and confirm password do not match'
+      );
+    }
+
+    // Check if the email is already taken
+    const emailTaken = await userService.isEmailTaken(email);
+    if (emailTaken) {
+      throw errorResponder(
+        errorTypes.EMAIL_ALREADY_TAKEN,
+        'Email already taken'
+      );
+    }
+
+    // Proceed with user creation
+    const success = await usersService.createUser(name, email, password);
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to create user'
+      );
+    }
+
+    return response.status(200).json({ name, email });
+  } catch (error) {
+    return next(error);
+  }
+}
